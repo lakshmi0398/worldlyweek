52df95c8bd2b72ac3c26d4b327471eeacac105b6
lakshmi0398 <lakshmiaviligonda30@gmail.com>
2024-02-29 03:30:21 +0000

    Message

---
+++ src/main/java/com/example/wordlyweek/WordlyWeekApplication.java
@@ -0,0 +1,11 @@
+package com.example.wordlyweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordlyWeekApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WordlyWeekApplication.class, args);
+    }
+}
